186261339880b55045189d311ef074b3ea0baca8
RafRamdan <rafiramdan2310@gmail.com>
2023-08-30 09:50:59 +0700

    Mengubah tampilan utama pada file master

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <a class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}" href="/dashboard">
+    <a class="nav-link {{ request()->is('dashboard*') ? 'active' : '' }}" href="/dashboard">
       <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
         <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <title>shop </title>
@@ -19,7 +19,7 @@
     </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link  {{ request()->is('customers') ? 'active' : '' }}" href="/customers">
+    <a class="nav-link  {{ request()->is('customers*') ? 'active' : '' }}" href="/customers">
       <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <title>office</title>
@@ -39,7 +39,7 @@
     </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ request()->is('produks') ? 'active' : '' }} " href="/produks">
+    <a class="nav-link {{ request()->is('produks*') ? 'active' : '' }} " href="/produks">
       <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
         <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <title>credit-card</title>
@@ -59,7 +59,7 @@
     </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ request()->is('transaksi') ? 'active' : '' }} " href="/transaksi">
+    <a class="nav-link {{ request()->is('transaksi*') ? 'active' : '' }} " href="/transaksi">
       <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <title>box-3d-50</title>
